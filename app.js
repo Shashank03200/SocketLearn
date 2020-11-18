22c9c2d08b6e5c23230371fa1ed97a3da825b6bc
Shashank03200 <shashank032000@gmail.com>
2020-11-18 12:11:43 +0530

    Test 1

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', function(socket){
+    console.log('A user connected');
+    socket.on('disconnect', function(){
+        console.log('A user disconnected');
+    })
+})
+
+http.listen(3000, ()=>{
+    console.log('Listening on port 3000.');
+})
